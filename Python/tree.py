e2e688887e7e6ae328c704f5556cffcfee1aed97
YuJinsoo <realaqua23@gmail.com>
2023-05-20 23:38:46 +0900

    부트 스트랩 예제 추가

--- Python/tree.py
+++ Python/tree.py
@@ -23,7 +23,7 @@ class Tree():
         new_node = Node(data)
         curr_node = self.root
         
-        while curr_node:
+        while curr_node != None:
             if curr_node.data == data:
                 return
             elif data < curr_node.data:
@@ -37,29 +37,37 @@ class Tree():
                 else:
                     curr_node.right = new_node
     
-    def get_childrens_data(self, node):
+    # 전체/하위 노드 출력
+    def get_childrens(self, node = None): 
         result = []
-        new_root = node
-        stack = [new_root]
         
-        while stack:
-            curr = stack.pop(0)
-            result.append(curr.data)
+        if node == None:
+            node = self.root
+            
+        new_root = node
+        queue = [new_root]
+        
+        while len(queue) != 0:
+            curr = queue.pop(0)
+            result.append(curr)
             
             if curr.left != None:
-                stack.append(curr.left)
+                queue.append(curr.left)
                 
             if curr.right != None:
-                stack.append(curr.right)
+                queue.append(curr.right)
                 
         
         return result
     
+    # 부모 노드
     def get_parent(self, node):
         result = 0
         stack = [self.root]
         
-        while node not in stack:
+        while len(stack) != 0:
+            if node == self.root:
+                break
             curr = stack.pop()
             
             if curr.left != None :
@@ -74,43 +82,22 @@ class Tree():
                 
         return result
     
-    def get_parent_data(self, node):
-        result = 0
-        stack = [self.root]
+    # 노드의 깊이
+    def get_node_depth(self, node): 
+        depth = 0# root를 0으로 하겠습니다.
+        curr_node = node
         
-        while node not in stack:
-            curr = stack.pop()
+        while curr_node != self.root:
+            curr_node = self.get_parent(curr_node)
+            depth += 1
             
-            if curr.left != None :
-                stack.append(curr.left)
-                if curr.left == node:
-                    result = curr.data
-            
-            if curr.right != None:
-                stack.append(curr.right)
-                if curr.right == node:
-                    result =  curr.data
-                
-        return result
+        return depth
     
     def left_child(self):
         return self.left
     
     def right_child(self):
         return self.right
-    
-    def remove(self, node):
-        
-        if node.left == None and node.right == None:
-            parent = self.get_parent(node)
-            if parent.data < node.data:
-                parent.right = None
-            elif parent.data > node.data:
-                parent.left = None
-        
-        if node.left != None and node.right == None:
-            maximum = max(self.get_childrens_data(node))
-                
     
     def find(self, data):
         result = None
@@ -120,7 +107,6 @@ class Tree():
             if curr.data == data:
                 result = curr
                 break
-                # curr = None # break
             elif curr.data > data:
                 curr = curr.left
             elif curr.data < data:
@@ -128,11 +114,37 @@ class Tree():
                 
         return result
     
+    # 제거 미완
+    def remove(self, node):
+        
+        if node.left == None and node.right == None:
+            if node == self.root:
+                self.root = None
+                return
+            
+            parent = self.get_parent(node)
+            if parent.data < node.data:
+                parent.right = None
+            elif parent.data > node.data:
+                parent.left = None
+        
+        if node.left != None and node.right == None:
+            parent = self.get_parent(node)
+            maximum = max(self.get_childrens_data(node))
+        
+        if node.left == None and node.right != None:
+            maximum = max(self.get_childrens_data(node))
+                
+        if node.left != None and node.right != None:
+            maximum = max(self.get_childrens_data(node))
+                
+    
+    
     def DFS(self):
         result = []
         stack = [self.root]
         
-        while stack:
+        while len(stack) != 0:
             curr = stack.pop()
             
             if curr.left != None:
@@ -148,7 +160,7 @@ class Tree():
         result = []
         queue = [self.root]
         
-        while queue:
+        while len(queue) != 0:
             curr = queue.pop(0)
             
             if curr.left != None:
@@ -175,12 +187,26 @@ print(t.root.right.left.data)
 print(t.DFS())
 print(t.BFS())
 
-print(t.get_childrens_data(t.root.left))
-print(t.get_childrens_data(t.root.right))
-print(t.get_parent_data(t.root.left))
-print(t.get_parent_data(t.root.left.right))
+for i in t.get_childrens(t.root.left):
+    print(i.data, end = ' ')
+    
+print()
+for i in t.get_childrens(t.root.right):
+    print(i.data, end = ' ')
+    
+print()
+for i in t.get_childrens():
+    print(i.data, end = ' ')
+
+print()
+print(t.get_parent(t.root.left).data)
+print(t.get_parent(t.root.left.right).data)
 
 print(t.find(3).data)
+print(t.find(3).data)
+
+print(t.get_node_depth(t.find(4)))
+
 
 
 
